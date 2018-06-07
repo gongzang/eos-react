4a057b7e8ddc63e81abdf0b15cd1c5abf86dee73
ming.zhang <ming.zhang@chinatalentgroup.com>
2018-06-07 16:57:28 +0800

    去掉多余的console.log

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,6 @@ class Home extends React.Component {
         const { menu: { activeItem }, match } = this.props;
         return (
             <div className='home-page'>
-                {console.log(this.props)}
                 <WrapMenu {...this.props}>
 
                 </WrapMenu>
